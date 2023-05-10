a3576a205f89c3f303a3088a2a551ca665d2620c
Jan Coufal <jan.coufal@gmail.com>
2023-05-10 22:50:08 +0200

    Basic skeleton (#3)
    * everything :P

--- app/src/main/java/cz/iddqd/smslocationping/MainActivity.kt
+++ app/src/main/java/cz/iddqd/smslocationping/MainActivity.kt
@@ -1,140 +1,125 @@
 package cz.iddqd.smslocationping
 
 import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import cz.iddqd.smslocationping.adapter.ContactAdapter
 import cz.iddqd.smslocationping.databinding.ActivityMainBinding
-import cz.iddqd.smslocationping.model.DataSource
 
 class MainActivity : AppCompatActivity() {
+	private val REQUIRED_PERMISSIONS = listOf(
+		Manifest.permission.RECEIVE_SMS,
+		Manifest.permission.READ_SMS,
+		Manifest.permission.SEND_SMS,
+		Manifest.permission.ACCESS_COARSE_LOCATION,
+		Manifest.permission.ACCESS_FINE_LOCATION,
+		Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+	)
 
 	private lateinit var binding : ActivityMainBinding
+	private lateinit var textNo : String
+	private lateinit var textYes : String
+	private lateinit var textOverallBad : String
+	private lateinit var textOverallGood : String
+	private lateinit var permissionElements : List<PermissionUiPack>
 
-	private val myNameIs = "I'm MainActivity"
-	private val intentFilter: IntentFilter = IntentFilter()
-	private var activityBroadcastReceiver: BroadcastReceiver? = null
-
-	init {
-		intentFilter.addAction("myAction1")
-		intentFilter.addAction("myAction2")
-	}
+	data class PermissionUiPack(val permission: String, val uiBinding: TextView, val name: String)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 
-		Log.d("EVENT", "onCreate")
+		Log.d(TAG, "onCreate")
 
-		val contactDataset = DataSource().loadContacts()
+		textNo = resources.getString(R.string.no)
+		textYes = resources.getString(R.string.yes)
+		textOverallBad = resources.getString(R.string.overall_status_bad)
+		textOverallGood = resources.getString(R.string.overall_status_good)
 
 		with(binding) {
-			btnSvcGetStatus.setOnClickListener(this@MainActivity::onSvcGetStatusClick)
-			btnSvcTurnOn.setOnClickListener(this@MainActivity::onClick)
-			btnSvcTurnOff.setOnClickListener(this@MainActivity::onClick)
+			btnRefreshStatus.setOnClickListener(this@MainActivity::onRefreshStatusClick)
 			btnDebug.setOnClickListener(this@MainActivity::onDebugClick)
-			with(recyclerView1) {
-				adapter = ContactAdapter(this@MainActivity, contactDataset)
-				setHasFixedSize(true)
-			}
+
+			permissionElements = listOf(
+				PermissionUiPack(Manifest.permission.RECEIVE_SMS, binding.textReceiveSms, resources.getString(R.string.receive_sms)),
+				PermissionUiPack(Manifest.permission.READ_SMS, binding.textReadSms, resources.getString(R.string.read_sms)),
+				PermissionUiPack(Manifest.permission.SEND_SMS, binding.textSendSms, resources.getString(R.string.send_sms)),
+				PermissionUiPack(Manifest.permission.ACCESS_COARSE_LOCATION, binding.textAccessCoarseLocation, resources.getString(R.string.access_coarse_location)),
+				PermissionUiPack(Manifest.permission.ACCESS_FINE_LOCATION, binding.textAccessFineLocation, resources.getString(R.string.access_fine_location)),
+				PermissionUiPack(Manifest.permission.ACCESS_BACKGROUND_LOCATION, binding.textAccessBackgroundLocation, resources.getString(R.string.access_background_location)),
+			)
 		}
 
-		bindBroadcastReceiver()
+		refreshUi()
 	}
 
 	override fun onPause() {
 		super.onPause()
-		Log.d("EVENT", "onPause")
+		Log.d(TAG, "onPause")
 	}
 
 	override fun onResume() {
 		super.onResume()
-		Log.d("EVENT", "onResume")
+		Log.d(TAG, "onResume")
+		refreshUi()
 	}
 
 	override fun onRestart() {
 		super.onRestart()
-		Log.d("EVENT", "onRestart")
+		Log.d(TAG, "onRestart")
+		refreshUi()
 	}
 
 	override fun onDestroy() {
 		super.onDestroy()
-		Log.d("EVENT", "onDestroy")
+		Log.d(TAG, "onDestroy")
 	}
 
-	private fun onClick(v: View?) {
-		Log.d("EVENT", "$v")
-	}
+	private fun onRefreshStatusClick(v: View?) {
+		Log.d(TAG, "onRefreshStatusClick($v)")
 
-	private fun onSvcGetStatusClick(v: View?) {
-		Log.d("EVENT", "onSvcGetStatusClick")
-		sendBroadcast(Intent("myAction1"))
-		sendBroadcast(Intent("myAction2"))
-		sendBroadcast(Intent("myAction3"))
+		// ensure permissions
+		val permissionMap = REQUIRED_PERMISSIONS.associateWith { ContextCompat.checkSelfPermission(baseContext, it) }
+		if (permissionMap.values.any { it != PackageManager.PERMISSION_GRANTED })
+			ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS.toTypedArray(), 666)
+
+		refreshUi()
 	}
 
 	private fun onDebugClick(v: View?) {
-		ensurePermissions()
+		Toast.makeText(this, ":P", Toast.LENGTH_LONG).show()
 	}
 
-	private fun ensurePermissions(): Boolean {
-		val requiredPermissions = listOf(
-			Manifest.permission.RECEIVE_SMS,
-			Manifest.permission.READ_SMS,
-			Manifest.permission.SEND_SMS,
-			Manifest.permission.ACCESS_COARSE_LOCATION,
-			Manifest.permission.ACCESS_FINE_LOCATION
-		)
-
-		if (missingAnyPermission(requiredPermissions))
-			askForPermissions(requiredPermissions)
-
-		return missingAnyPermission(requiredPermissions)
-	}
-
-	private fun missingAnyPermission(requiredPermissions: List<String>): Boolean {
-		Log.d("ASSERT", "permissionCheck")
-
-		val missingAnyPermission = requiredPermissions.stream()
-			.peek { p -> Log.d("ASSERT", "permission $p") }
-			.map { p -> ContextCompat.checkSelfPermission(baseContext, p) }
-			.peek { p ->
-				Log.d(
-					"ASSERT",
-					"...$p (" + (if (p == PackageManager.PERMISSION_GRANTED) "ok" else "denied") + ")"
-				)
-			}
-			.anyMatch { p -> p != PackageManager.PERMISSION_GRANTED }
-
-		Log.d("MAIN", "missingAnyPermission $missingAnyPermission")
-
-		return missingAnyPermission
-	}
-
-	private fun askForPermissions(requiredPermissions: List<String>) {
-		ActivityCompat.requestPermissions(this, requiredPermissions.toTypedArray(), 666)
-	}
-
-	private fun bindBroadcastReceiver() {
-		if (activityBroadcastReceiver == null) {
-			activityBroadcastReceiver = object : BroadcastReceiver() {
-				override fun onReceive(context: Context?, intent: Intent?) {
-					Log.d(TAG, "activity::myNameIs $myNameIs")
-					Thread.sleep(1_000)
-					Log.d(TAG, "$myNameIs: activityBroadcastReceiver.onReceive(context: $context, intent: $intent)")
-				}
-			}.also { registerReceiver(it, intentFilter) }
+	@SuppressLint("SetTextI18n")
+	private fun refreshUi() {
+		fun updateTextView(v: TextView, text: String, permissionGranted: Boolean) {
+			v.text = text
+			v.setTextColor(if(permissionGranted) Color.BLACK else Color.RED)
 		}
+
+		fun updatePermissionTextView(v: TextView, title: String, permissionGranted: Boolean) {
+			updateTextView(v, "$title ${if(permissionGranted) textYes else textNo}", permissionGranted)
+		}
+
+		Log.d(TAG, "refreshUi()")
+
+		var overallStatus = true
+		permissionElements.forEach {
+			val permissionGranted = ContextCompat.checkSelfPermission(baseContext, it.permission) == PackageManager.PERMISSION_GRANTED
+			overallStatus = overallStatus && permissionGranted
+			updatePermissionTextView(it.uiBinding, it.name, permissionGranted)
+		}
+
+		updateTextView(binding.textOverallStatus, if(overallStatus) textOverallGood else textOverallBad, overallStatus)
 	}
 
 	companion object {
